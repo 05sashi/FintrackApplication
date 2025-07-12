bab7adbcb57e4eb61b62bda4fe41eced11b9ee09
05sashi <putlurusasidharreddy@gmail.com>
2025-07-12 20:55:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,6 +17,10 @@ app.use('/api/transactions', transactionRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/ping', (req, res) => {
+  res.status(200).send('OK');
+});
+
 const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
